2baafd682f551615f854d097a97a2ce2d719a1d6
Nicholas Tow <nxtr1114@gmail.com>
2023-03-25 10:56:40 -0500

    cleanup

--- Config.cs
+++ Config.cs
@@ -2,22 +2,20 @@ using System.Text.Json;
 
 //Config.cs
 
-public class LoopingCommand
+public class CustomCommand
 {
     public string Command { get; set; }
     public bool Loop { get; set; }
     public int Interval { get; set; }
     public int Delay { get; set; } // new property
 }
-
-
 public class Config
 {
     public string ConsoleTitle { get; set; }
     public string JavaExecutable { get; set; }
     public string ServerJar { get; set; }
     public string JvmArguments { get; set; }
-    public List<LoopingCommand> Commands { get; set; }
+    public List<CustomCommand> Commands { get; set; }
 
     public static void CreateDefaultConfig()
     {
@@ -27,13 +25,12 @@ public class Config
             JavaExecutable = "java",
             ServerJar = "server.jar",
             JvmArguments = "-Xmx2G -Xms2G",
-            Commands = new List<LoopingCommand>
+            Commands = new List<CustomCommand>
             {
-                new LoopingCommand { Command = "say This command will run after 10 seconds", Loop = false, Interval = 0, Delay = 10 },
-                new LoopingCommand { Command = "say This command will only execute once", Loop = false, Interval = 0 }
+                new CustomCommand { Command = "say This command will run after 10 seconds", Loop = false, Interval = 0, Delay = 10 },
+                new CustomCommand { Command = "say This command will only execute once", Loop = false, Interval = 0 }
             }
         };
-
         var configJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
         File.WriteAllText("config.json", configJson);
     }
